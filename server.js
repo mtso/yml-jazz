2d4fa47d299e4a7841c78dc2dcc46efdccada944
mtso <mtso@tuta.io>
2017-03-30 10:48:27 -0700

    Make server standalone and render for each request

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const http = require('http')
 const fs = require('fs')
+const yaml = require('js-yaml')
+const jazz = require('jazz')
 const config = require('./config')
-const _ = require('./build')
+const encoding = 'utf8'
+
+// Save rendered html in case of errors
+var cache = 'Uninitialized, check console logs. Problem is probably in "content.yml" or "index.template"';
 
 const server = http.createServer(function(_, res) {
-  const html = fs.readFileSync(config.OUTPUT_FILE)
-  res.end(html)
+  try {
+    var content = fs.readFileSync(config.CONTENT_FILE, encoding)
+    content = {content: yaml.load(content)}
+    var template = fs.readFileSync(config.TEMPLATE_FILE, encoding)
+    template = jazz.compile(template)
+    template.eval(content, function(data) {
+      cache = data
+      res.end(data)
+    })
+  } catch(err) {
+    console.log(err)
+    return res.end(cache)
+  }
 })
 
 server.listen(process.env.PORT || 3000)
